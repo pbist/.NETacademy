867aab2554ddd401db906c37a04957732abd6311
pbist <paulina.bistula@hotmail.com>
2016-06-12 23:32:53 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace ContactBook
+{
+    public partial class MainWindow : Window
+    {
+        public Person newContact;
+
+        private ObservableCollection<Person> contactList = new ObservableCollection<Person>();
+        private int index;
+
+        private Option option;
+        enum Option
+        {
+            Add,
+            Edit
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            listBox.DataContext = contactList;
+        }
+
+        public void setButtons(bool option = false)
+        {
+            newBtn.IsEnabled = option;
+            editBtn.IsEnabled = option;
+        }
+
+        public void addContact()
+        {
+            if (option == Option.Add)
+            {
+                contactList.Add(newContact);
+            }
+            else if (option == Option.Edit)
+            {
+                contactList[index] = newContact;
+            }
+        }
+
+        private void NewContact(object sender, RoutedEventArgs e)
+        {
+            option = Option.Add;
+            newContact = new Person();
+            setButtons();
+            (new NameWin(this)).Show();
+        }
+
+        private void EditContact(object sender, RoutedEventArgs e)
+        {
+            index = listBox.SelectedIndex;
+            if (index != -1)
+            {
+                option = Option.Edit;
+                newContact = contactList[index];
+                setButtons();
+                (new NameWin(this)).Show();
+            }
+        }
+
+        private void DeleteContact(object sender, RoutedEventArgs e)
+        {
+            int index = listBox.SelectedIndex;
+            if (index != -1)
+                contactList.RemoveAt(index);
+        }
+
+        private void OnClose(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactBook
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string name;
+        private string surname;
+        private string address;
+        private string number;
+
+        // properties
+        public string Name
+        {
+            get { return name; }
+            set { name = value; OnPropertyChanged("Name"); }
+        }
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; OnPropertyChanged("Surname"); }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; OnPropertyChanged("Address"); }
+        }
+        public string Number
+        {
+            get { return number; }
+            set { number = value; OnPropertyChanged("Number"); }
+        }
+
+        protected void OnPropertyChanged(string info)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(info));
+            }
+        }
+    }
+}
